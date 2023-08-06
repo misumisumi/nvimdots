e80b8f90acb952faf408586642ec71af33df241f
Sumi-Sumi <dragon511southern@gmail.com>
2023-08-07 06:11:09 +0900

    feat(user/keymap): fix core.mapping

--- lua/user/keymap/init.lua
+++ lua/user/keymap/init.lua
@@ -3,6 +3,7 @@ local plug_map = {}
 plug_map = vim.tbl_extend(
 	"force",
 	plug_map,
+	require("user.keymap.core"),
 	require("user.keymap.editor"),
 	require("user.keymap.tool"),
 	require("user.keymap.ui").plug_map
