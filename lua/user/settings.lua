38fccedbda8d7de0272f7bc060fed6dbb05f53a6
Sumi-Sumi <dragon511southern@gmail.com>
2023-08-11 23:23:39 +0900

    update: settigs and plugin config

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -2,7 +2,10 @@ local settings = {}
 
 -- Example
 settings["use_ssh"] = true
-settings["lsp_deps"] = { "rnix" }
+settings["lsp_deps"] = {
+	"rnix",
+	"texlab",
+}
 settings["disabled_plugins"] = {
 	"m4xshen/autoclose.nvim",
 	"nvim-tree/nvim-tree.lua",
