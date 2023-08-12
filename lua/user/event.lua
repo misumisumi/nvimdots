f5597d8913b415a67f4728a463ae7938712725fa
Sumi-Sumi <dragon511southern@gmail.com>
2023-08-12 18:29:40 +0900

    update(user): Update configs

--- lua/user/event.lua
+++ lua/user/event.lua
@@ -2,6 +2,7 @@ local definitions = {
 	-- Example
 	bufs = {
 		{ "BufWritePre", "COMMIT_EDITMSG", "setlocal noundofile" },
+		{ "BufRead", "*.tfvars", "set filetype=terraform" },
 		-- Auto toggle fcitx5
 		{ "VimEnter", "* :silent", "!fcitx5-remote -g EN" },
 		{ "FocusGained", "* :silent", "!fcitx5-remote -g EN" },
--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -3,7 +3,9 @@ local settings = {}
 -- Example
 settings["use_ssh"] = true
 settings["lsp_deps"] = {
+	"jsonls",
 	"rnix",
+	"terraformls",
 	"texlab",
 }
 settings["disabled_plugins"] = {
