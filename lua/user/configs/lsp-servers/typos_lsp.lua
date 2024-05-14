ce072d742742c8411550140400de8d4e4d5d60ca
misumisumi <dragon511southern@gmail.com>
2024-05-14 10:05:25 +0900

    fix(typos_lsp): add root_dir
    https://github.com/tekumara/typos-lsp/issues/29

--- lua/user/configs/lsp-servers/typos_lsp.lua
+++ lua/user/configs/lsp-servers/typos_lsp.lua
@@ -1,4 +1,9 @@
 return {
 	cmd = { "typos-lsp" },
 	filetypes = { "*" },
+	root_dir = function(fname)
+		return require("lspconfig.util").root_pattern("typos.toml", "_typos.toml", ".typos.toml", "pyproject.toml")(
+			fname
+		) or vim.fn.getcwd()
+	end,
 }
