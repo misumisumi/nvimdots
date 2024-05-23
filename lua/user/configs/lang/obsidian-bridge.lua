a4d00e639f5fb63ec5fad09f377a6d4bbbbfe1c2
misumisumi <dragon511southern@gmail.com>
2024-05-23 23:44:09 +0900

    feat: add obtain.nvim

---
+++ lua/user/configs/lang/obsidian-bridge.lua
@@ -0,0 +1,6 @@
+return {
+	obsidian_server_address = "http://localhost:27123",
+	-- This is not stable yet.
+	-- More details: https://github.com/coddingtonbear/obsidian-local-rest-api/discussions/75
+	scroll_sync = false,
+}
--- lua/user/plugins/lang.lua
+++ lua/user/plugins/lang.lua
@@ -1,5 +1,34 @@
 local lang = {}
 
+lang["epwalsh/obsidian.nvim"] = {
+	version = "*",
+	lazy = true,
+	ft = "markdown",
+	cmd = {
+		"ObsidianDailies",
+		"ObsidianNew",
+		"ObsidianOpen",
+		"ObsidianSearch",
+		"ObsidianTags",
+		"ObsidianToday",
+		"ObsidianTomorrow",
+		"ObsidianYesterday",
+		"ObsidianNewFromTemplate",
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "oflisback/obsidian-bridge.nvim", enabled = false, config = require("user.configs.lang.obsidian-bridge") },
+	},
+	config = require("user.configs.lang.obsidian"),
+}
+lang["HakonHarnes/img-clip.nvim"] = {
+	enabled = false,
+	lazy = true,
+	ft = { "markdown", "tex", "typst", "rst", "asciidoc" },
+	keys = {
+		{ "<leader><leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+	},
+}
 lang["chrisbra/csv.vim"] = {
 	lazy = true,
 	ft = "csv",
