223552e1fd5933fbf6a1e9fcd1f4aa504548d45d
misumisumi <dragon511southern@gmail.com>
2024-07-14 18:25:41 +0900

    fix(typos_lsp): fix root dir

--- lua/user/configs/lsp-servers/typos_lsp.lua
+++ lua/user/configs/lsp-servers/typos_lsp.lua
@@ -1,9 +1,4 @@
 return {
 	cmd = { "typos-lsp" },
 	filetypes = { "*" },
-	root_dir = function(fname)
-		return require("lspconfig.util").root_pattern("typos.toml", "_typos.toml", ".typos.toml", "pyproject.toml")(
-			fname
-		) or vim.fn.getcwd()
-	end,
 }
