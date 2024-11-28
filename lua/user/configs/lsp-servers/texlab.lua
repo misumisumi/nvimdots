74d5e715a6e46f5f36711b677c3668343c94013d
misumisumi <dragon511southern@gmail.com>
2024-11-29 03:25:48 +0900

    fix(texlab): fix texlab config

--- lua/user/configs/lsp-servers/texlab.lua
+++ lua/user/configs/lsp-servers/texlab.lua
@@ -1,5 +1,6 @@
 return {
-	cmd = { "texlab" },
+	-- cmd = { "texlab" },
+	cmd = { "texlab", "-vv", "--log-file=./texlab.log" },
 	filetypes = { "tex", "plaintex", "bib" },
 	root_dir = require("lspconfig.util").root_pattern(
 		".latexmkrc",
@@ -10,29 +11,37 @@ return {
 		".git"
 	),
 	settings = {
-		{
-			texlab = {
-				auxDirectory = ".",
-				bibtexFormatter = "texlab",
-				build = {
-					args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
-					executable = "latexmk",
-					forwardSearchAfter = false,
-					onSave = false,
-				},
-				chktex = {
-					onEdit = false,
-					onOpenAndSave = false,
-				},
-				diagnosticsDelay = 300,
-				formatterLineLength = 80,
-				forwardSearch = {
-					args = {},
-				},
-				latexFormatter = "latexindent",
-				latexindent = {
-					modifyLineBreaks = false,
-				},
+		texlab = {
+			auxDirectory = ".",
+			bibtexFormatter = "texlab",
+			build = {
+				args = { "-g" },
+				executable = "latexmk",
+				forwardSearchAfter = true,
+				onSave = false,
+			},
+			chktex = {
+				onEdit = true,
+				onOpenAndSave = true,
+			},
+			diagnosticsDelay = 300,
+			formatterLineLength = 80,
+			forwardSearch = function()
+				if require("core.global").is_windows then
+					return {
+						args = { "-reuse-instance", "%p", "-forward-search", "%f", "%l" },
+						executable = require("core.global").home .. "/AppData/Local/SumatraPDF/SumatraPDF.exe",
+					}
+				else
+					return {
+						args = { "--synctex-forward", "%l:1:%f", "%p" },
+						executable = "zathura",
+					}
+				end
+			end,
+			latexFormatter = "latexindent",
+			latexindent = {
+				modifyLineBreaks = false,
 			},
 		},
 	},
