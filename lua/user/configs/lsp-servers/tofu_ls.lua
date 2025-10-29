6a612395fa4bbb2f7c64d9bca9a1704c6effbd11
misumisumi <s.kobayashi@misumi-sumi.com>
2025-10-29 19:20:42 +0900

    fix(user): remove openbrowser and replace terraformls to tofu_ls

---
+++ lua/user/configs/lsp-servers/tofu_ls.lua
@@ -0,0 +1,5 @@
+return {
+	cmd = { "tofu-ls", "serve" },
+	filetypes = { "terraform", "terraform-vars", "opentofu", "opentofu-vars" },
+	root_pattern = { ".terraform", ".git" },
+}
--- lua/user/configs/null-ls.lua
+++ lua/user/configs/null-ls.lua
@@ -3,6 +3,9 @@ return function()
 	local btns = null_ls.builtins
 	local sources = {
 		btns.diagnostics.statix,
+		btns.formatting.terraform_fmt.with({
+			command = "tofu",
+		}),
 	}
 	local exception = require("user.configs.exception")
 	if exception.is_nixos() then
