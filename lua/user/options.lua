2a7d263c52b8591cb0e082033d8bc6a9c12e3eb8
misumisumi <s.kobayashi@misumi-sumi.com>
2025-11-27 08:46:33 +0900

    fix(user/options): set osc52 clipboard in SSH session

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,6 @@
 vim.g.editorconfig = 0
 
--- vim.env.WEZTERM_EXECUTABLEに'mux-server'を含んでいるか判定
-if vim.env.WEZTERM_EXECUTABLE and vim.env.WEZTERM_EXECUTABLE:find("mux%-server") then
+local function set_osc52_clipboard()
 	local function paste()
 		return {
 			vim.fn.split(vim.fn.getreg(""), "\n"),
@@ -22,6 +21,33 @@ if vim.env.WEZTERM_EXECUTABLE and vim.env.WEZTERM_EXECUTABLE:find("mux%-server")
 		cache_enabled = 0,
 	}
 end
+
+-- vim.env.WEZTERM_EXECUTABLEに'mux-server'を含んでいるか判定
+local function check_wezterm_remote_clipboard(callback)
+	local wezterm_executable = vim.uv.os_getenv("WEZTERM_EXECUTABLE")
+
+	if wezterm_executable and wezterm_executable:find("wezterm-mux-server", 1, true) then
+		callback(true) -- Remote WezTerm session found
+	else
+		callback(false) -- No remote WezTerm session
+	end
+end
+
+-- Schedule the setting after `UiEnter` because it can increase startup-time.
+vim.schedule(function()
+	vim.opt.clipboard:append("unnamedplus")
+
+	-- Standard SSH session handling
+	if vim.uv.os_getenv("SSH_CLIENT") ~= nil or vim.uv.os_getenv("SSH_TTY") ~= nil then
+		set_osc52_clipboard()
+	else
+		check_wezterm_remote_clipboard(function(is_remote_wezterm)
+			if is_remote_wezterm then
+				set_osc52_clipboard()
+			end
+		end)
+	end
+end)
 
 local options = {
 	-- Example
