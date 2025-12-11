38b76bd95e0b87b7089bc759b77ae3df448595c5
misumisumi <s.kobayashi@misumi-sumi.com>
2025-12-12 03:20:40 +0900

    fix(user/event): fcitx5 events are added when is_linux

--- lua/user/event.lua
+++ lua/user/event.lua
@@ -2,12 +2,19 @@ local definitions = {
 	-- Example
 	bufs = {
 		{ "BufWritePre", "COMMIT_EDITMSG", "setlocal noundofile" },
-		-- Auto toggle fcitx5
-		{ "VimEnter", "* :silent", "!fcitx5-remote -g EN" },
-		{ "FocusGained", "* :silent", "!fcitx5-remote -g EN" },
-		{ "FocusLost", "* :silent", "!fcitx5-remote -g SKK" },
-		{ "VimLeave", "* :silent", "!fcitx5-remote -g SKK" },
 	},
 }
+local fcitx5 = {
+	-- Auto toggle fcitx5
+	{ "VimEnter", "* :silent", "!fcitx5-remote -g EN" },
+	{ "FocusGained", "* :silent", "!fcitx5-remote -g EN" },
+	{ "FocusLost", "* :silent", "!fcitx5-remote -g SKK" },
+	{ "VimLeave", "* :silent", "!fcitx5-remote -g SKK" },
+}
+if require("core.global").is_linux then
+	for k, v in pairs(fcitx5) do
+		definitions.bufs[k] = v
+	end
+end
 
 return definitions
