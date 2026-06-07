6e72ade2909250ee32f28b7060740ce5b373900f
misumisumi <s.kobayashi@misumi-sumi.com>
2026-06-07 22:30:30 +0900

    fix: fix settings by upstream change

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -51,22 +51,7 @@ settings["format_timeout"] = 5000
 
 settings["use_copilot"] = true
 settings["use_chat"] = true
-settings["chat_models"] = function()
-	return {
-		"google/gemini-2.5-flash", -- default
-		"google/gemini-2.5-pro",
-		"google/gemini-3-pro",
-		"copilot/gpt-5-mini",
-		"copilot/gpt-5-codex-mini",
-		"copilot/gpt-5-codex-max",
-		"gemini/default",
-		-- free models
-		"moonshotai/kimi-k2:free",
-		"qwen/qwen3-coder:free",
-		"deepseek/deepseek-chat-v3-0324:free",
-		"deepseek/deepseek-r1:free",
-		"google/gemma-3-27b-it:free",
-	}
-end
+settings["ai_adapters"] = {}
+settings["edit_prediction_source"] = "copilot"
 
 return settings
